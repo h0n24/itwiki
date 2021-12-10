ab2a8c8c018a151fb4b4a19eadbe483d13287bd1
Jan Šablatura <h0n24.cz@gmail.com>
2021-12-10 21:24:41 +0100

    i18n deepl en

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
-// const withNextra = require('nextra')({
-//   theme: 'nextra-theme-docs',
-//   themeConfig: './theme.config.js',
-//   unstable_staticImage: true,
-//   i18n: {
-//     locales: ['cs', 'en'],
-//     defaultLocale: 'cs',
-//   },
-// })
-
-// module.exports = withNextra()
-
-
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',

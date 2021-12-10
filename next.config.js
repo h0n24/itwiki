096a63750b006a3104ae571cc88b1169ed198a33
Jan Šablatura <h0n24.cz@gmail.com>
2021-12-10 21:19:06 +0100

    i18n test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,26 @@
+// const withNextra = require('nextra')({
+//   theme: 'nextra-theme-docs',
+//   themeConfig: './theme.config.js',
+//   unstable_staticImage: true,
+//   i18n: {
+//     locales: ['cs', 'en'],
+//     defaultLocale: 'cs',
+//   },
+// })
+
+// module.exports = withNextra()
+
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
+});
+
+module.exports = withNextra({
   i18n: {
     locales: ['cs', 'en'],
-    defaultLocale: 'cs',
-  },
-})
-module.exports = withNextra()
+    defaultLocale: 'cs'
+  }
+});
+

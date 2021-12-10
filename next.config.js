63bf0bafb8064a3c52b7d71679784eca1df84f40
Jan Šablatura <h0n24.cz@gmail.com>
2021-12-10 20:51:37 +0100

    i18n test

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
+  i18n: {
+    locales: ['cs', 'en'],
+    defaultLocale: 'cs',
+  },
 })
 module.exports = withNextra()

3c7bce1721f10b6276b524eca0280b71a9a401bc
Jan Šablatura <h0n24.cz@gmail.com>
2021-12-12 21:36:00 +0100

    test 2

--- next.config.js
+++ next.config.js
@@ -2,32 +2,13 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
-});
+})
 
 module.exports = withNextra({
   i18n: {
-    locales: ['default','cs', 'en'],
+    locales: ['default', 'cs', 'en'],
     defaultLocale: 'default',
     localeDetection: false,
   },
   trailingSlash: true,
-  async redirects() {
-    return JSON.parse(
-      JSON.stringify([
-        {
-          source: '/default',
-          destination: '/cs',
-          locale: false,
-          permanent: false,
-        },
-        {
-          source: '/default/(!public/):slug*',
-          destination: '/cs/:slug*',
-          locale: false,
-          permanent: false,
-        },
-      ])
-    );
-  },
-});
-
+})
